19176413ab131e65ea78d2e966566f55be4b2f60
Andrei Nicholson <contact@andreinicholson.com>
2020-02-06 22:11:48 -0500

    Fix mixed content error

--- src/app/items/shared/algolia-api.service.ts
+++ src/app/items/shared/algolia-api.service.ts
@@ -9,7 +9,7 @@ import { User } from './user.model';
 
 @Injectable()
 export class AlgoliaApiService {
-  private readonly baseUrl: string = 'http://hn.algolia.com/api/v1';
+  private readonly baseUrl: string = 'https://hn.algolia.com/api/v1';
 
   constructor(private http: HttpClient) { }
 
