c30299540c802f0a9e6cc79826040f06c605b655
Andrei Nicholson <contact@andreinicholson.com>
2020-11-03 23:02:22 -0500

    Angular 10.2.1

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  title: string = 'Angular 9 HN';
+  title: string = 'Angular 10 HN';
 
   constructor() { }
 
